e069dcd8beec42255a72bab5a8ffa1e8e0aaf549
bNolions <bnolions@h-tech.co>
2022-01-25 15:58:22 +0800

    [feature] doc

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # AniGamerAppUIMock
+
+## UI 示意圖
+
+***Navigation Bar***
+
+![navigationBar](./docs/assets/navigationbar.png)
+
+***Home***
+
+![home](./docs/assets/home.png)
+
+***Search***
+
+![search](./docs/assets/search.png)
+
+***Collect***
+
+![Collect](./docs/assets/collect.png)
+
+***Record***
+
+![record](./docs/assets/record.png)
+
+***My***
+
+![My](./docs/assets/My.png)
---
+++ app/src/main/java/tw/nolions/anigamerappuimock/widget/HeaderView.kt
@@ -0,0 +1,25 @@
+package tw.nolions.anigamerappuimock.widget
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import tw.nolions.anigamerappuimock.ui.theme.AniGamerAppUIMockTheme
+
+@Composable
+fun PageHeader(title: String) {
+    Text(
+        text = title,
+        fontWeight = FontWeight.Bold,
+        fontSize = 20.sp,
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PageHeaderPreview() {
+    AniGamerAppUIMockTheme {
+        PageHeader("紀錄")
+    }
+}
