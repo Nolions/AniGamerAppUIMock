5090d1309805ceeb94aa656bfb4226ef1bf36613
bNolions <bnolions@h-tech.co>
2022-01-27 16:48:09 +0800

    [feature] 整合NavigationBar& Main views

--- app/src/main/java/tw/nolions/anigamerappuimock/MainActivity.kt
+++ app/src/main/java/tw/nolions/anigamerappuimock/MainActivity.kt
@@ -3,36 +3,75 @@ package tw.nolions.anigamerappuimock
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.tooling.preview.Preview
 import tw.nolions.anigamerappuimock.ui.theme.AniGamerAppUIMockTheme
+import tw.nolions.anigamerappuimock.view.SearchView
+import tw.nolions.anigamerappuimock.view.VideoFavoriteView
+import tw.nolions.anigamerappuimock.view.VideoRecordsView
+import tw.nolions.anigamerappuimock.view.home.HomeView
+import tw.nolions.anigamerappuimock.widget.NavigationBar
+import tw.nolions.anigamerappuimock.widget.Screen
 
 class MainActivity : ComponentActivity() {
+    @ExperimentalFoundationApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             AniGamerAppUIMockTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    MainScreen()
                 }
             }
         }
     }
 }
 
+@ExperimentalFoundationApi
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MainScreen() {
+    val screenState = remember { mutableStateOf(Screen.HOME) }
+
+    Scaffold(
+        bottomBar = { NavigationBar(state = screenState) },
+        content = { content(screenState) }
+    )
 }
 
+@ExperimentalFoundationApi
+@Composable
+fun content(state: MutableState<Screen>) {
+    when (state.value) {
+        Screen.HOME -> {
+            HomeView()
+        }
+        Screen.SEARCH -> {
+            SearchView()
+        }
+        Screen.FAVORITE -> {
+            VideoFavoriteView()
+        }
+        Screen.RECORD -> {
+            VideoRecordsView()
+        }
+        Screen.MY -> {
+        }
+    }
+}
+
+@ExperimentalFoundationApi
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     AniGamerAppUIMockTheme {
-        Greeting("Android")
+        MainScreen()
     }
 }
